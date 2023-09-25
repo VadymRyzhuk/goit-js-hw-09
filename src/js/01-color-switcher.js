9c93a0c33e086dba0061b41853d784e5b0bee7cf
VadymRyzhuk <ceasarj571@gmail.com>
2023-09-25 18:37:25 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let body = document.body;
+
+let timerId;
+
+startBtn.addEventListener("click", () => {
+    startBtn.disabled = true;
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+});
+
+
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+})
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+
 
